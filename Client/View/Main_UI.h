08a551975a68938bf59dc8696b5353f9d94434e0
码lun <114496166+code-lun@users.noreply.github.com>
2024-06-19 13:01:29 +0800

    change

---
+++ Client/View/Main_UI.h
@@ -0,0 +1,12 @@
+/*************************************************************************
+	>    File Name: Main_UI.h
+	>       Author: lun
+	> Created Time: 2024年06月12日
+ ************************************************************************/
+#ifndef _MAIN_UI_H
+#define _MAIN_UI_H
+
+void Main_UI_Hello();
+
+void Main_UI_Menu();
+#endif
